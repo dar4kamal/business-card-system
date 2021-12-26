e8d351599f12e9c5faa4e814c34de1dbe4872540
dar4kamal <sasaa9646@gmail.com>
2021-12-26 12:57:07 +0200

    testing pug view engine SSR

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
+import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 import { UniversalExceptionFilter } from './utility/universal.filter';
 import { TransformInterceptor } from './utility/transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('pug');
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new UniversalExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(5000);
 }
 bootstrap();
